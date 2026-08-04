9a8822cbc59aa39d59723b085120fb5fb901f74c
Thunder Dragon <163322214+ThunDerDra@users.noreply.github.com>
2026-08-04 15:48:50 +0530

    Add files via upload

---
+++ ledbrightnesscontrol.ino
@@ -0,0 +1,61 @@
+int buttonpin = 12;
+int ledpin = 9;
+int dt = 50;
+int brightup = 10;
+int brightdown = 11;
+int brightness = 255;
+
+bool currentstate;
+bool laststate = HIGH;
+bool ledstate = false;
+bool brightinc;
+bool brightdec;
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  pinMode(buttonpin, INPUT);
+  Serial.begin(9600);
+  digitalWrite(ledpin, LOW);
+
+}
+
+void loop() {
+  currentstate = digitalRead(buttonpin);
+  brightinc = digitalRead(brightup);
+  brightdec = digitalRead(brightdown);
+    
+  Serial.println(currentstate);
+  delay(dt);
+
+  if (laststate == HIGH  &&  currentstate == LOW) {
+    ledstate = !ledstate;
+    if(ledstate){ 
+    analogWrite(ledpin, brightness);
+    }
+    else {
+    analogWrite(ledpin, 0);
+    }
+    delay(dt);
+  } 
+
+  laststate = currentstate;
+
+  if (brightinc == LOW  &&  brightness < 255) { 
+    if(ledstate){
+    brightness += 17;
+    analogWrite(ledpin, brightness);
+    }
+  }
+
+  if (brightdec == LOW &&  brightness > 0) {
+    if(ledstate){
+    brightness -= 17;
+    analogWrite(ledpin, brightness);
+    }
+  }
+
+
+
+
+
+}
